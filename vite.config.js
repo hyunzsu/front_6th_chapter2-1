02ce76acb7085773dc5587e3d07ee74cbc8e72aa
현지수 <hyunzsu@naver.com>
2025-08-01 08:38:16 +0900

    chore: 배포 에러 해결

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/front_6th_chapter2-1/" : "/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js',
+    environment: "jsdom",
+    setupFiles: "src/setupTests.js",
   },
 });
